0eaea64c2862294a9307a28f04f2c41e21063ce2
Josh Black-Star <23347017+jdblackstar@users.noreply.github.com>
2023-10-19 10:44:48 -0700

    first commit, most binary and unary ops

---
+++ punytorch.py
@@ -0,0 +1,59 @@
+import numpy as np
+
+
+class Tensor:
+    def __init__(self, data) -> None:
+        self.data = data if isinstance(data, np.ndarray) else np.array(data)
+
+    """
+    BINARY OPS
+    """
+
+    def __add__(self, other) -> "Tensor":
+        return Tensor(self.data + other.data)
+
+    def __sub__(self, other) -> "Tensor":
+        return Tensor(self.data - other.data)
+
+    def __mul__(self, other) -> "Tensor":
+        return Tensor(self.data * other.data)
+
+    def __truediv__(self, other) -> "Tensor":
+        return Tensor(self.data / other.data)
+
+    def __mod__(self, other) -> "Tensor":
+        return Tensor(self.data % other.data)
+
+    def __pow__(self, other) -> "Tensor":
+        return Tensor(self.data**other.data)
+    
+    def __ge__(self, other) -> 'Tensor':
+        return Tensor(self.data >= other.data)
+
+    def __eq__(self, other) -> 'Tensor':
+        return Tensor(self.data == other.data)
+
+    def __ne__(self, other) -> 'Tensor':
+        return Tensor(self.data != other.data)
+    
+    """
+    UNARY OPS
+    """
+    
+    def __abs__(self) -> 'Tensor':
+        return Tensor(np.abs(self.data))
+
+    def __neg__(self) -> 'Tensor':
+        return Tensor(-self.data)
+
+    def __invert__(self) -> 'Tensor':
+        return Tensor(~self.data)
+
+    def __repr__(self) -> str:
+        return f"tensor({self.data})"
+
+
+if __name__ == "__main__":
+    x = Tensor([8])
+    y = Tensor([5])
+    z = x + y
