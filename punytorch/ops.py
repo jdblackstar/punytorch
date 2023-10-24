7321f460d37c78cac6d275421bce5d03401e99e1
Josh Black-Star <23347017+jdblackstar@users.noreply.github.com>
2023-10-24 11:39:05 -0700

    ops classes tests

---
+++ punytorch/ops.py
@@ -0,0 +1,187 @@
+import numpy as np
+
+
+class Function:
+    def __init__(self, op, *args):
+        self.op = op
+        self.args = args
+
+
+class Add:
+    @staticmethod
+    def forward(x, y):
+        """
+        z = x + y
+        """
+        return x + y
+
+    @staticmethod
+    def backward(context, grad):
+        """
+        d(x + y)/dx = 1
+        d(x + y)/dy = 1
+
+        return [1] for both x and y
+        """
+        x, y = context.args
+        return [1], [1]
+
+
+class Sub:
+    @staticmethod
+    def forward(x, y):
+        """
+        z = x - y
+        """
+        return x - y
+
+    @staticmethod
+    def backward(context, grad):
+        """
+        d(x - y)/dx = 1
+        d(x - y)/dy = -1
+
+        return [1] for x
+        return [-1] for y
+        """
+        x, y = context.args
+        return [1], [-1]
+
+
+class Mul:
+    @staticmethod
+    def forward(x, y):
+        """
+        z = x * y
+        """
+        return x * y
+
+    @staticmethod
+    def backward(context, grad):
+        """
+        d(x * y)/dx = y
+        d(x * y)/dy = x
+
+        return (y.data * grad.data) for x
+        return (x.data * grad.data) for y
+        """
+        x, y = context.args
+        return (y.data * grad.data), (x.data * grad.data)
+
+
+class TrueDiv:
+    @staticmethod
+    def forward(x, y):
+        """
+        z = x / y
+        """
+        return x / y
+
+    @staticmethod
+    def backward(context, grad):
+        """
+        d(x / y)/dx = 1/y
+        d(x / y)/dy = -x/y^2
+
+        return (grad.data / y.data) for x
+        return (-grad.data * x.data / (y.data ** 2)) for y
+        """
+        x, y = context.args
+        return (np.array(grad.data) / y.data), (-np.array(grad.data) * x.data / (y.data**2))
+
+
+class Mod:
+    """
+    WARNING: The modulus operation is not differentiable at integer points,
+    and the derivative with respect to `y` is undefined. This implementation
+    assumes that the gradient with respect to `y` is 0, which is not universally
+    accepted. It also checks if all elements in `y.data` are integers and raises
+    a ValueError if they're not. This is a simplification and may not be suitable
+    for all use cases.
+    """
+
+    @staticmethod
+    def forward(x, y):
+        """
+        z = x % y
+        """
+        return x % y
+
+    @staticmethod
+    def backward(context, grad):
+        """
+        d(x % y)/dx = 1
+        d(x % y)/dy = 0
+
+        return an array of ones like x.data for x
+        return an array of zerose like y.data for y
+        """
+        x, y = context.args
+        if not np.all(y.data.astype(int) == y.data):
+            raise ValueError(
+                "Backward operation for modulus is only defined when y is an integer."
+            )
+        return (np.ones_like(x.data)), (np.zeros_like(y.data))
+
+
+class Pow:
+    @staticmethod
+    def forward(x, y):
+        """
+        z = x ^ y
+        """
+        return x**y
+
+    @staticmethod
+    def backward(context, grad):
+        """
+        d(x ^ y)/dx = y * x^(y - 1)
+        d(x ^ y)/dy = x^y * log(x)
+
+        return (y.data * x.data ** (y.data - 1)) for x
+        return (x.data**y.data * np.log(x.data)) for y
+        """
+        x, y = context.args
+        return (y.data * x.data ** (y.data - 1)), (x.data**y.data * np.log(x.data))
+
+
+class MatMul:
+    @staticmethod
+    def forward(x, y):
+        """
+        z = x @ y
+        """
+        return x @ y  # @ is the matrix multiplication operator in Python
+
+    @staticmethod
+    def backward(context, grad):
+        """
+        If Z = X @ Y, then
+        d(Z)/dX = grad @ Y.T
+        d(Z)/dY = X.T @ grad
+        """
+        x, y = context.args
+
+        # Shape Check
+        assert (
+            x.data.shape[1] == y.data.shape[0]
+        ), "Incompatible shapes for matrix multiplication"
+        assert (
+            grad.shape[0] == x.data.shape[0]
+        ), "Incompatible shapes for backward matrix multiplication"
+        assert (
+            grad.shape[1] == y.data.shape[1]
+        ), "Incompatible shapes for backward matrix multiplication"
+
+        # Dimension Check
+        assert (
+            x.data.ndim >= 2 and y.data.ndim >= 2
+        ), "Both inputs to matmul should be at least 2-dimensional"
+        assert grad.ndim >= 2, "The gradient should be at least 2-dimensional"
+
+        # Non-emptiness Check
+        assert (
+            x.data.size > 0 and y.data.size > 0 and grad.size > 0
+        ), "Inputs to matmul should not be empty"
+
+        return grad @ y.data.T, x.data.T @ grad
---
+++ tests/test_ops.py
@@ -0,0 +1,46 @@
+from punytorch.tensor import Tensor
+import numpy as np
+
+def test_Add():
+    x = Tensor([8])
+    y = Tensor([5])
+    z = x + y
+    assert z.data == 13
+    z.backward()
+    assert np.all(x.grad == np.array([1]))
+
+def test_Sub():
+    x = Tensor([8])
+    y = Tensor([5])
+    z = x - y
+    assert z.data == 3
+    z.backward()
+    assert np.all(x.grad == np.array([1]))
+
+def test_Mul():
+    x = Tensor([8])
+    y = Tensor([5])
+    z = x * y
+    assert z.data == 40
+    z.backward()
+    assert x.grad == y.data
+    assert y.grad == x.data
+
+def test_TrueDiv():
+    x = Tensor([8])
+    y = Tensor([2])
+    z = x / y
+    assert z.data == 4
+    z.backward()
+    assert x.grad == 1 / y.data
+    assert y.grad == -x.data / (y.data ** 2)
+
+def test_MatMul():
+    x = Tensor(np.array([[1, 2, 3], [4, 5, 6]]))  # 2x3 matrix
+    y = Tensor(np.array([[7, 8], [9, 10], [11, 12]]))  # 3x2 matrix
+    z = x @ y
+    assert np.all(z.data == np.array([[58, 64], [139, 154]]))
+    z.grad = np.ones_like(z.data)  # Set an initial gradient for z
+    z.backward()
+    assert np.allclose(x.grad, np.array([[15, 19, 23], [15, 19, 23]]))
+    assert np.allclose(y.grad, np.array([[5, 5], [7, 7], [9, 9]]))
