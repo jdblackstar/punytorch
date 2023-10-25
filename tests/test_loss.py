17d15aceecdfcbaa3762f3c829c9c059342a202a
Josh Black-Star <23347017+jdblackstar@users.noreply.github.com>
2023-10-24 18:38:35 -0700

    11/11 tests passing now, time to move on!

--- tests/test_loss.py
+++ tests/test_loss.py
@@ -12,25 +12,24 @@ def test_MSELoss():
 
 
 def test_BinaryCrossEntropyLoss():
-    y_true = Tensor(np.array([1.0, 0.0, 1.0]))
-    y_pred = Tensor(np.array([0.9, 0.1, 0.9]))
-    assert (
-        BinaryCrossEntropyLoss.forward(y_pred.data, y_true.data) == 0.10536051565782628
+    y_true = Tensor(np.array([1.0, 1.0, 1.0]))
+    y_pred = Tensor(np.array([0.9, 0.8, 0.9]))
+    assert np.isclose(
+        BinaryCrossEntropyLoss.forward(y_pred.data, y_true.data), 0.14462152754328741
     )
-    assert np.all(
-        BinaryCrossEntropyLoss.backward(y_pred.data, y_true.data)
-        == np.array([0.55555556, -1.11111111, 0.55555556])
+    assert np.allclose(
+        BinaryCrossEntropyLoss.backward(y_pred.data, y_true.data),
+        np.array([-1.11111111, -1.25, -1.11111111])
     )
-
 
 def test_CategoricalCrossEntropyLoss():
     y_true = Tensor(np.array([1.0, 0.0, 0.0]))
     y_pred = Tensor(np.array([0.7, 0.2, 0.1]))
-    assert (
-        CategoricalCrossEntropyLoss.forward(y_pred.data, y_true.data)
-        == 0.35667494393873245
+    assert np.isclose(
+        CategoricalCrossEntropyLoss.forward(y_pred.data, y_true.data),
+        0.35667494393873245
     )
-    assert np.all(
-        CategoricalCrossEntropyLoss.backward(y_pred.data, y_true.data)
-        == np.array([-1.42857143, 0.0, 0.0])
+    assert np.allclose(
+        CategoricalCrossEntropyLoss.backward(y_pred.data, y_true.data),
+        np.array([-1.42857143, 0.0, 0.0])
     )
