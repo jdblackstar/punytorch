824220fd681a86f37b9c014d0acd6d26126af5f2
Josh Black-Star <23347017+jdblackstar@users.noreply.github.com>
2023-12-29 16:47:35 -0500

    did my napkin math wrong :)

--- tests/test_tensor.py
+++ tests/test_tensor.py
@@ -13,7 +13,7 @@ def test_backpropagation():
     x = Tensor([1.0, 2.0, 3.0], requires_grad=True)
     y = Tensor([4.0, 5.0, 6.0], requires_grad=True)
     z = x * y + y
-    assert z.data.tolist() == [5.0, 12.0, 21.0]
+    assert z.data.tolist() == [8.0, 15.0, 24.0]
     z.backward()
     assert x.grad.tolist() == [4.0, 5.0, 6.0]
-    assert y.grad.tolist() == [2.0, 4.0, 6.0]
+    assert y.grad.tolist() == [2.0, 3.0, 4.0]
