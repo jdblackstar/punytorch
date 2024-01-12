ebaadf06d40e26c8217ae49bfd22cdf9f3df8a17
Josh Black-Star <23347017+jdblackstar@users.noreply.github.com>
2024-01-12 12:51:01 -0800

    change to work with one-hot encoded labels

--- punytorch/losses.py
+++ punytorch/losses.py
@@ -26,9 +26,7 @@ class CrossEntropyLoss:
     def backward(y_pred, y_true):
         exps = np.exp(y_pred - np.max(y_pred, axis=1, keepdims=True)) + 1e-22
         probs = exps / np.sum(exps, axis=1, keepdims=True)
-        d_loss = np.zeros_like(probs)
-        d_loss[np.arange(len(y_true)), y_true.astype(int)] -= 1
-        d_loss += probs
+        d_loss = probs - y_true  # Subtract the one-hot encoded labels
         d_loss /= len(y_true)
         return Tensor(d_loss)  # Wrap the result in a Tensor
 
