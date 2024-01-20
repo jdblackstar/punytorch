3b4839213dbcb957f05a285ce3499c0b8634b9c8
Josh Black-Star <23347017+jdblackstar@users.noreply.github.com>
2024-01-19 23:59:48 -0800

    fixed the tests for my loss functions after fiddling with stuff

--- tests/test_loss.py
+++ tests/test_loss.py
@@ -9,8 +9,8 @@ from punytorch.tensor import Tensor
 
 
 def test_MSELoss():
-    y_true = Tensor(np.array([1.0, 2.0, 3.0]))
-    y_pred = Tensor(np.array([1.0, 2.0, 3.0]))
+    y_true = Tensor([1.0, 2.0, 3.0])
+    y_pred = Tensor([1.0, 2.0, 3.0])
     assert np.isclose(MSELoss.forward(y_pred.data, y_true.data).data, Tensor(0.0).data)
     assert np.allclose(
         MSELoss.backward(y_pred.data, y_true.data).data, Tensor([0.0, 0.0, 0.0]).data
@@ -18,10 +18,11 @@ def test_MSELoss():
 
 
 def test_BinaryCrossEntropyLoss():
-    y_true = Tensor(np.array([1.0, 1.0, 1.0]))
-    y_pred = Tensor(np.array([0.9, 0.8, 0.9]))
+    y_true = Tensor([1.0, 1.0, 1.0])
+    y_pred = Tensor([0.9, 0.8, 0.9])
     assert np.isclose(
-        BinaryCrossEntropyLoss.forward(y_pred.data, y_true.data), 0.14462152754328741
+        BinaryCrossEntropyLoss.forward(y_pred.data, y_true.data).data,
+        0.14462152754328741,
     )
     assert np.allclose(
         BinaryCrossEntropyLoss.backward(y_pred.data, y_true.data),
@@ -30,10 +31,10 @@ def test_BinaryCrossEntropyLoss():
 
 
 def test_CategoricalCrossEntropyLoss():
-    y_true = Tensor(np.array([1.0, 0.0, 0.0]))
-    y_pred = Tensor(np.array([0.7, 0.2, 0.1]))
+    y_true = Tensor([1.0, 0.0, 0.0])
+    y_pred = Tensor([0.7, 0.2, 0.1])
     assert np.isclose(
-        CategoricalCrossEntropyLoss.forward(y_pred.data, y_true.data),
+        CategoricalCrossEntropyLoss.forward(y_pred.data, y_true.data).data,
         0.35667494393873245,
     )
     assert np.allclose(
